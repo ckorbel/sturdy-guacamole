1631f8fa48b3013cae3dd1cf91a46e37f184d44c
Chris Korbel <ckorbel@Chriss-MacBook-Pro.local>
2019-07-30 22:53:46 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+//ITodo[]  = demand: [{ text: "value", complete: true }, { text: "value", complete: false }];
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 
 const App: React.FC = () => {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>{value}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </Fragment>
   );
-}
+};
 
 export default App;
