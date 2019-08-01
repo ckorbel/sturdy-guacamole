6cd478d2844d4c66edd937605a3553459eb77021
Chris Korbel <ckorbel@uhoklucas.inside.fender>
2019-08-01 08:45:46 -0700

    use Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,14 @@
 import React, { Fragment, useState } from "react";
 import "./App.css";
+import { Store } from "./Store";
 
-type FormElem = React.FormEvent<HTMLFormElement>;
-
-//ITodo[]  = demand: [{ text: "value", complete: true }, { text: "value", complete: false }];
-interface ITodo {
-  text: string;
-  complete: boolean;
-}
-
-const App: React.FC = () => {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  const handleSubmit = (e: FormElem): void => {
-    e.preventDefault();
-    addTodo(value);
-    setValue("");
-  };
-
-  const addTodo = (text: string): void => {
-    const newTodos: ITodo[] = [...todos, { text, complete: false }];
-    setTodos(newTodos);
-  };
-
-  const completeTodo = (index: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos[index].complete = !newTodos[index].complete;
-    setTodos(newTodos);
-  };
-
-  const removeTodo = (index: number): void => {
-    const newTodos: ITodo[] = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
+const App: React.FC = (): JSX.Element => {
+  const [state, dispatch] = React.useContext(Store);
   return (
-    <Fragment>
-      <h1>{value}</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
-      <section>
-        {todos.map((todo: ITodo, index: number) => (
-          <Fragment key={index}>
-            <div
-              style={{ textDecoration: todo.complete ? "line-through" : "" }}
-            >
-              {todo.text}
-            </div>
-            <button type="button" onClick={() => completeTodo(index)}>
-              {todo.complete ? "Incomplete" : "Complete"}
-            </button>
-            <button type="button" onClick={() => removeTodo(index)}>
-              Remove
-            </button>
-          </Fragment>
-        ))}
-      </section>
-    </Fragment>
+    <React.Fragment>
+      <h1>The Office</h1>
+      <p>Pick your favorite!</p>
+    </React.Fragment>
   );
 };
 
---
+++ src/Todo.tsx
@@ -0,0 +1,70 @@
+import React, { Fragment, useState } from "react";
+import "./App.css";
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+//ITodo[]  = demand: [{ text: "value", complete: true }, { text: "value", complete: false }];
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+const Todo: React.FC = () => {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <h1>{value}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}>
+            <div
+              style={{ textDecoration: todo.complete ? "line-through" : "" }}
+            >
+              {todo.text}
+            </div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {todo.complete ? "Incomplete" : "Complete"}
+            </button>
+            <button type="button" onClick={() => removeTodo(index)}>
+              Remove
+            </button>
+          </Fragment>
+        ))}
+      </section>
+    </Fragment>
+  );
+};
+
+export default Todo;
